ac61d8d6a9105d7158ce0beee539fdb9bf5c0149
Giselle Georges <giselle@nextintl.com>
2026-07-31 14:14:13 -0400

    categories design

--- app/dashboard/category/page.tsx
+++ app/dashboard/category/page.tsx
@@ -7,10 +7,10 @@ const page = () => {
    return (
      <div style={{ display: "flex", height: "100vh", overflow: "hidden", background: "#F8FAFF" }}>
        <WorkOrderSidebar />
-    
+     <main style={{ flex: 1, display: "flex", overflow: "hidden" }}>
          <CategoriesPage />
        
-       
+       </main>
      </div>
    );
 }
