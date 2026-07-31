0f28ddddcfd0a75c49e23e4c98f6431ec5267142
Giselle Georges <giselle@nextintl.com>
2026-07-31 14:08:39 -0400

    reporting page

--- app/dashboard/category/page.tsx
+++ app/dashboard/category/page.tsx
@@ -7,9 +7,9 @@ const page = () => {
    return (
      <div style={{ display: "flex", height: "100vh", overflow: "hidden", background: "#F8FAFF" }}>
        <WorkOrderSidebar />
-       <main style={{ flex: 1, display: "flex", overflow: "hidden" }}>
+    
          <CategoriesPage />
-       </main>
+       
        
      </div>
    );
