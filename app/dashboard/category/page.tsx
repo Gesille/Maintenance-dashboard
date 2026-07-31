4981146fa7f5cbf72f5af7671e89c41c0a0ba4cb
Giselle Georges <giselle@nextintl.com>
2026-07-31 14:06:33 -0400

    reporting page

--- app/dashboard/category/page.tsx
+++ app/dashboard/category/page.tsx
@@ -1,16 +1,18 @@
-import { CategoriesPage } from '@/component/Categoriespage'
-import { WorkOrderSidebar } from '@/component/Sidebar'
-import React from 'react'
+import { CategoriesPage } from '@/component/Categoriespage';
+import { WorkOrderSidebar } from '@/component/Sidebar';
+
 
 const page = () => {
-  return (
-    <div>
+ 
+   return (
+     <div style={{ display: "flex", height: "100vh", overflow: "hidden", background: "#F8FAFF" }}>
+       <WorkOrderSidebar />
+       <main style={{ flex: 1, display: "flex", overflow: "hidden" }}>
          <CategoriesPage />
-<WorkOrderSidebar />
+       </main>
        
-      
-    </div>
-  )
+     </div>
+   );
 }
 
 export default page
